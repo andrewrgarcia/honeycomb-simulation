559ed8f9aefe7b147e0d53dac93f7f80d5c67103
Andrew Garcia <garcia.gtr@gmail.com>
2024-11-04 12:52:32 -0500

    update readme and links

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer() {
     return (
       <footer style={{ textAlign: 'center', padding: '1em', fontSize: '0.9em', color: '#555' }}>
         © {(new Date().getFullYear())} Andrew R. Garcia. Created using 
-        <a href="https://example.com" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '4px', color: 'orange', textDecoration: 'none' }}>
-          MakeFilesHere</a> from <a href="https://example.com" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '4px', color: 'orange', textDecoration: 'none' }}>files-here-wzrd</a> and ChatGPT.
+        <a href="https://github.com/andrewrgarcia/files-here-wzrd/blob/main/MakeFilesHere.sh" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '4px', color: 'orange', textDecoration: 'none' }}>
+          MakeFilesHere</a> from <a href="https://github.com/andrewrgarcia/files-here-wzrd" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '4px', color: 'orange', textDecoration: 'none' }}>files-here-wzrd</a> and ChatGPT.
       </footer>
     );
   }
